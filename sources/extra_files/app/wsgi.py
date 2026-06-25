62c2a829d876338ca0ecacff433702967f0cb2ed
tanios43 <tanioslehero@hotmail.com>
2026-06-25 04:10:56 +0200

    Fix: wsgi:application pour sous-chemin /mvpt4

--- sources/extra_files/app/wsgi.py
+++ sources/extra_files/app/wsgi.py
@@ -1,2 +1,11 @@
 from app import app, init_db
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.wrappers import Response
+
 init_db()
+
+# Permet à Flask de fonctionner sous le sous-chemin /mvpt4
+application = DispatcherMiddleware(
+    Response('Not Found', status=404),
+    {'/mvpt4': app}
+)
